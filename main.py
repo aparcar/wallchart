8e9722876594bbe1c0bddab7f897dcca8c349e58
Paul Spooren <mail@aparcar.org>
2021-07-10 18:08:57 -1000

    backup
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from peewee import ForeignKeyField
 from peewee import IntegerField
 from peewee import DeferredForeignKey
 from peewee import DateField
+from peewee import CompositeKey
+from peewee import AutoField
+from peewee import JOIN
 
 from flask import Flask
 from flask import g
@@ -20,8 +23,17 @@ from flask import render_template
 from functools import wraps
 from hashlib import sha256
 from pathlib import Path
+from slugify import slugify
+from datetime import date
+
 
 import csv
+
+import logging
+
+# logger = logging.getLogger("peewee")
+# logger.addHandler(logging.StreamHandler())
+# logger.setLevel(logging.DEBUG)
 
 
 DATABASE = "wallcharts.db"
@@ -44,14 +56,22 @@ class Worker(BaseModel):
     email = CharField(unique=True, null=True)
     phone = IntegerField(unique=True, null=True)
     notes = TextField(null=True)
-    contract = IntegerField()
-    department = DeferredForeignKey("Department", backref="department")
+    contract = CharField()
+    unit = CharField()
+    department_id = IntegerField()
     active = BooleanField(default=True)
+    added = DateField(default=date.today)
+    updated = DateField()
+
+    class Meta:
+        indexes = ((("name", "unit", "department_id", "contract"), True),)
 
 
 class Department(BaseModel):
-    name = CharField(unique=True)
-    chair = ForeignKeyField(Worker, backref="chair", null=True)
+    id = IntegerField(primary_key=True)
+    name = CharField()
+    chair = ForeignKeyField(Worker, field="id", backref="chair", null=True)
+    slug = CharField()
 
 
 class User(BaseModel):
@@ -60,23 +80,21 @@ class User(BaseModel):
     email = CharField(unique=True)
 
 
-class StructureTests(BaseModel):
+class StructureTest(BaseModel):
     name = CharField(unique=True)
     description = TextField()
-    active = BooleanField()
-    date = DateField()
+    active = BooleanField(default=True)
+    added = DateField(default=date.today)
 
 
 class Participation(BaseModel):
-    worker = ForeignKeyField(Worker)
-    structure_test = ForeignKeyField(StructureTests)
+    worker = ForeignKeyField(Worker, field="id")
+    structure_test = ForeignKeyField(StructureTest)
 
 
 def create_tables():
     with database:
-        database.create_tables(
-            [Worker, Department, User, StructureTests, Participation]
-        )
+        database.create_tables([Worker, Department, User, StructureTest, Participation])
 
 
 def auth_user(user):
@@ -147,9 +165,67 @@ def login():
 
 
 @app.route("/departments/")
+@login_required
 def departments():
     departments = Department.select().order_by(Department.name.desc())
     return object_list("departments.html", departments, "department_list")
+
+
+@app.route("/workers/<path:department_slug>")
+@login_required
+def workers(department_slug):
+    department = Department.get(Department.slug == department_slug)
+
+    workers = (
+        Worker.select()
+        .join(Participation, JOIN.LEFT_OUTER, on=(Worker.id == Participation.worker))
+        .where(Worker.department_id == department.id or "*")
+        .order_by(Worker.name)
+    )
+    structure_tests = StructureTest.select().order_by(StructureTest.added)
+    print(structure_tests)
+
+    return object_list(
+        "workers.html",
+        workers,
+        "worker_list",
+        worker_count=len(workers),
+        department=department,
+        structure_test_list=structure_tests,
+    )
+
+
+@app.route("/structure_tests", methods=["GET", "POST"])
+@login_required
+def structure_tests():
+    if request.method == "POST":
+        structure_test, created = StructureTest.get_or_create(
+            name=request.form["name"], description=request.form["description"]
+        )
+        if created:
+            flash("Structure Test added")
+        else:
+            flash("Structure test already exists")
+
+    structure_tests = StructureTest.select().order_by(StructureTest.added)
+    return object_list("structure_tests.html", structure_tests, "structure_tests_list")
+
+
+@app.route("/workers/edit/<int:worker_id>", methods=["GET", "POST"])
+@login_required
+def workers_edit(worker_id):
+    if request.method == "POST":
+        Worker.update(
+            {
+                Worker.email: request.form["email"],
+                Worker.phone: request.form["phone"],
+                Worker.notes: request.form["notes"],
+            }
+        ).where(Worker.id == worker_id).execute()
+        flash("Worker data updated")
+
+    worker = Worker.get(Worker.id == worker_id)
+    return render_template("workers_edit.html", worker=worker)
 
 
 @app.route("/logout/")
@@ -162,27 +238,36 @@ def logout():
 def parse_csv():
     with open("roster.csv", newline="") as csvfile:
         reader = csv.DictReader(csvfile, delimiter=";")
+
         for row in reader:
             print(row)
-            department = Department.get_or_create(name=row["Dept ID Desc"].title())
-            worker = Worker.update(
+            department, created = Department.get_or_create(
+                id=row["Dept ID"],
+                name=row["Dept ID Desc"].title(),
+                slug=slugify(row["Dept ID Desc"]),
+            )
+            if created:
+                print(slugify(row["Dept ID Desc"]))
+            # flash(f"New Department added: {department.name}")
+            Worker.get_or_create(
                 name=row["Name"],
-                contract=int(row["Job Code"][2:3]),
-                department=department,
+                contract=row["Job Code"],
+                department_id=row["Dept ID"],
+                unit=row["Unit"],
+                updated=date.today(),
             )
 
 
 if __name__ == "__main__":
-    create_tables()
-    parse_csv()
-    # User.get_or_create(
-    #    username="admin",
-    #    password=sha256("admin".encode("utf-8")).hexdigest(),
-    #    email="admin@admin.com",
-    # )
+    if not Path(DATABASE).exists():
+        create_tables()
+        parse_csv()
+    User.get_or_create(
+        username="admin",
+        password=sha256("admin".encode("utf-8")).hexdigest(),
+        email="admin@admin.com",
+    )
 
-    # for department in Path("departments").read_text().splitlines():
-    #    print(department)
-    #    Department.get_or_create(name=department.title())
+    Participation.get_or_create(worker=1281, structure_test=1)
 
     app.run()
