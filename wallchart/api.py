ecf5d4dd433301a7a57ef74b0457b3ec68abb995
Paul Spooren <mail@aparcar.org>
2021-09-11 19:59:47 -1000

    api: add units and worker endpoints
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- wallchart/api.py
+++ wallchart/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
+from playhouse.flask_utils import get_object_or_404
 
-from wallchart.db import Department, Participation, Worker
+from wallchart.db import Department, Participation, Unit, Worker
 from wallchart.util import login_required
 
 api = Blueprint("api", __name__, url_prefix="/api")
@@ -22,6 +23,23 @@ def api_workers():
     )
 
 
+@api.route("/worker/<int:worker_id>")
+@login_required
+def api_worker(worker_id):
+    workers = (
+        Worker.select(
+            Worker,
+            Department.slug.alias("department_slug"),
+            Department.name.alias("department_name"),
+        )
+        .join(Department, on=(Worker.organizing_dept_id == Department.id))
+        .dicts()
+    )
+
+    worker = get_object_or_404(workers, (Worker.id == worker_id))
+    return jsonify(worker)
+
+
 @api.route("/participation")
 @login_required
 def api_participation():
@@ -38,3 +56,9 @@ def api_participation():
 @login_required
 def api_departments():
     return jsonify(list(Department.select().dicts()))
+
+
+@api.route("/units")
+@login_required
+def api_units():
+    return jsonify(list(Unit.select().dicts()))
